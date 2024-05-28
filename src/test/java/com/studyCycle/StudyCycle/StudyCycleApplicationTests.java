0e656d380ae402b9bc752be60aa0c88f8da209b7
Ankit <ankitvitale18@gmail.com>
2024-05-28 16:18:05 +0530

    initial

---
+++ src/test/java/com/studyCycle/StudyCycle/StudyCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyCycle.StudyCycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
