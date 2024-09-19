e640f0ae1996f8e88074bf2b5286fc8bad523004
ankitvitale <74039291+ankitvitale@users.noreply.github.com>
2024-09-19 19:05:27 +0530

    Update CorsConfiguration.java

--- src/main/java/com/studyCycle/StudyCycle/Configuration/CorsConfiguration.java
+++ src/main/java/com/studyCycle/StudyCycle/Configuration/CorsConfiguration.java
@@ -1,26 +1,26 @@
-package com.studyCycle.StudyCycle.Configuration;
+// package com.studyCycle.StudyCycle.Configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class CorsConfiguration {
-    private static final String GET = "GET";
-    private static final String POST = "POST";
-    private static final String PUT = "PUT";
-    private static final String DELETE = "DELETE";
+// public class CorsConfiguration {
+//     private static final String GET = "GET";
+//     private static final String POST = "POST";
+//     private static final String PUT = "PUT";
+//     private static final String DELETE = "DELETE";
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-               registry.addMapping("/**")
-                .allowedOrigins("*")  // Allow all origins
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-            }
-        };
-    }
-}
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurer() {
+//             @Override
+//             public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                 .allowedOrigins("*")  // Allow all origins
+//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                 .allowedHeaders("*")
+//                 .allowCredentials(true);
+//             }
+//         };
+//     }
+// }
